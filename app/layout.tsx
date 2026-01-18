e4623a67a65868d5eb76a3d6a81ca9c46176aca4
Rihards Voskans <rihardsvoskans@Rihardss-MacBook-Air.local>
2026-01-18 20:13:33 +0000

    Describe what you changed

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,6 +12,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head><meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" /></head>
       <body>{children}</body>
     </html>
   )
